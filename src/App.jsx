8194e7a103ec31249e6a08d111916bdb46ccd572
José Segura <jose.segura.ms@gmail.com>
2026-06-18 23:05:48 -0300

    feat: spring easing nas transitions, orbes animados no fundo e textura de grid

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ import Main from '@components/Main'
 function App() {
   return (
     <>
+      <div className="bg-orbs" aria-hidden="true">
+        <span className="bg-orb bg-orb--1" />
+        <span className="bg-orb bg-orb--2" />
+        <span className="bg-orb bg-orb--3" />
+      </div>
       <Header />
       <Main />
       <Footer />
